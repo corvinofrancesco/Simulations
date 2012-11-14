079180de20b9779a2d5ca82be76d8c58b45858e3
Corvino Francesco <corvinofrancesco@alice.it>
2012-11-14 15:04:59 +0100

    #3 PhetApplication: remove configuration function
    remove:
    * possibility to override parsering of arguments
    * possibility to override creation of phetframe

--- phetcommon/src/main/java/edu/colorado/phet/common/phetcommon/application/PhetApplication.java
+++ phetcommon/src/main/java/edu/colorado/phet/common/phetcommon/application/PhetApplication.java
@@ -29,24 +29,20 @@ public class PhetApplication
     //----------------------------------------------------------------
     // Class data
     //----------------------------------------------------------------
-
     public static final String DEVELOPER_CONTROLS_COMMAND_LINE_ARG = "-dev";//Command line argument to enable developer-only features
     private static ArrayList<PhetApplication> phetApplications = new ArrayList<PhetApplication>();
-
     //----------------------------------------------------------------
     // Instance data
     //----------------------------------------------------------------
-
     private ITabbedModulePane tabbedModulePane;
     private PhetApplicationConfig phetApplicationConfig;
-
     private PhetFrame phetFrame;
     private ModuleManager moduleManager;
     private PhetAboutDialog aboutDialog; // not null only when About dialog is shown for the first time
     private boolean applicationStarted = false;//flag to make sure we don't start the application more than once
-
     //The exit strategy, to be applied when the user closes the application.  By default it calls PhetExit.exit
     private VoidFunction0 exitStrategy = new VoidFunction0() {
+
         public void apply() {
             PhetExit.exit();
         }
@@ -55,29 +51,25 @@ public class PhetApplication
     //----------------------------------------------------------------
     // Constructors
     //----------------------------------------------------------------
-
-    public PhetApplication( PhetApplicationConfig config ) {
-        this( config, new JTabbedModulePane() );
+    public PhetApplication(PhetApplicationConfig config) {
+        this(config, new JTabbedModulePane());
     }
 
-    protected PhetApplication( PhetApplicationConfig phetApplicationConfig, ITabbedModulePane tabbedPane ) {
+    protected PhetApplication(PhetApplicationConfig phetApplicationConfig, ITabbedModulePane tabbedPane) {
         this.phetApplicationConfig = phetApplicationConfig;
         this.tabbedModulePane = tabbedPane;
 
-        this.moduleManager = new ModuleManager( this );
-        phetFrame = createPhetFrame();
-        phetApplicationConfig.getFrameSetup().initialize( phetFrame );
+        this.moduleManager = new ModuleManager(this);
+        phetFrame = new PhetFrame(this);
+        phetApplicationConfig.getFrameSetup().initialize(phetFrame);
 
-        // Handle command line arguments
-        parseArgs( phetApplicationConfig.getCommandLineArgs() );
 
-        phetApplications.add( this );
+        phetApplications.add(this);
     }
 
     //----------------------------------------------------------------
     // 
     //----------------------------------------------------------------
-
     public ITabbedModulePane getTabbedModulePane() {
         return tabbedModulePane;
     }
@@ -88,13 +80,12 @@ public class PhetApplication
      * @return true or false
      */
     public boolean isDeveloperControlsEnabled() {
-        return CommandLineUtils.contains( phetApplicationConfig.getCommandLineArgs(), DEVELOPER_CONTROLS_COMMAND_LINE_ARG );
+        return CommandLineUtils.contains(phetApplicationConfig.getCommandLineArgs(), DEVELOPER_CONTROLS_COMMAND_LINE_ARG);
     }
 
     public ISimInfo getSimInfo() {
         return phetApplicationConfig;
     }
-
 
     /**
      * Get the last created PhetApplication.
@@ -102,26 +93,7 @@ public class PhetApplication
      * @return last created PhetApplication.
      */
     public static PhetApplication getInstance() {
-        return (PhetApplication) phetApplications.get( phetApplications.size() - 1 );
-    }
-
-    /**
-     * Creates the PhetFrame for the application
-     * <p/>
-     * Concrete subclasses implement this
-     *
-     * @return The PhetFrame
-     */
-    protected PhetFrame createPhetFrame() {
-        return new PhetFrame( this );
-    }
-
-    /**
-     * Processes command line arguments. May be extended by subclasses.
-     *
-     * @param args
-     */
-    protected void parseArgs( String[] args ) {
+        return (PhetApplication) phetApplications.get(phetApplications.size() - 1);
     }
 
     /**
@@ -130,39 +102,19 @@ public class PhetApplication
      * Sets up the mechanism that sets the reference sizes of all ApparatusPanel2 instances.
      */
     public void startApplication() {
-        if ( !applicationStarted ) {
+        if (!applicationStarted) {
             applicationStarted = true;
-            if ( moduleManager.numModules() == 0 ) {
-                throw new RuntimeException( "No modules in module manager" );
+            if (moduleManager.numModules() == 0) {
+                throw new RuntimeException("No modules in module manager");
             }
 
-            // Set up a mechanism that will set the reference sizes of all ApparatusPanel2 instances
-            // after the PhetFrame has been set to its startup size.
-            // When the outer WindowAdapter gets called, the PhetFrame is
-            // at the proper size, but the ApparatusPanel2 has not yet gotten its resize event.
-            phetFrame.addWindowFocusListener( new WindowAdapter() {
-                public void windowGainedFocus( WindowEvent e ) {
-                    initializeModuleReferenceSizes();
-                    phetFrame.removeWindowFocusListener( this );
-                }
-            } );
-
-            // #3395: dev feature, start with the module number specified on the command line.
-            if ( isDeveloperControlsEnabled() ) {
-                String startModuleNumber = phetApplicationConfig.getOptionArg( "-startModule" );
-                if ( startModuleNumber != null ) {
-                    setStartModule( getModule( Integer.valueOf( startModuleNumber ) ) );
-                }
-            }
-
-            moduleManager.setActiveModule( getStartModule() );
-            phetFrame.setVisible( true );
+            moduleManager.setActiveModule(getStartModule());
+            phetFrame.setVisible(true);
 
             updateLogoVisibility();
-        }
-        else {
+        } else {
             //TODO: put into standard logging framework
-            System.out.println( "WARNING: PhetApplication.startApplication was called more than once." );
+            System.out.println("WARNING: PhetApplication.startApplication was called more than once.");
         }
     }
 
@@ -175,16 +127,16 @@ public class PhetApplication
      * This method and functionality will be removed if is too awkward in practice (for example, too confusing to override this default).
      */
     protected void updateLogoVisibility() {
-        for ( int i = 0; i < moduleManager.numModules(); i++ ) {
-            if ( moduleAt( i ).isLogoPanelVisible() && phetFrame.getTabbedModulePane() != null && phetFrame.getTabbedModulePane().getLogoVisible() ) {
-                moduleAt( i ).setLogoPanelVisible( false );
+        for (int i = 0; i < moduleManager.numModules(); i++) {
+            if (moduleAt(i).isLogoPanelVisible() && phetFrame.getTabbedModulePane() != null && phetFrame.getTabbedModulePane().getLogoVisible()) {
+                moduleAt(i).setLogoPanelVisible(false);
             }
         }
     }
 
     private void initializeModuleReferenceSizes() {
-        for ( int i = 0; i < moduleManager.numModules(); i++ ) {
-            ( moduleManager.moduleAt( i ) ).setReferenceSize();
+        for (int i = 0; i < moduleManager.numModules(); i++) {
+            (moduleManager.moduleAt(i)).setReferenceSize();
         }
     }
 
@@ -200,14 +152,13 @@ public class PhetApplication
     //----------------------------------------------------------------
     // Module-related methods
     //----------------------------------------------------------------
-
     /**
      * Sets the modules in the application
      *
      * @param modules
      */
-    public void setModules( Module[] modules ) {
-        moduleManager.setModules( modules );
+    public void setModules(Module[] modules) {
+        moduleManager.setModules(modules);
     }
 
     /**
@@ -215,8 +166,8 @@ public class PhetApplication
      *
      * @param module the Module to remove.
      */
-    public void removeModule( Module module ) {
-        moduleManager.removeModule( module );
+    public void removeModule(Module module) {
+        moduleManager.removeModule(module);
     }
 
     /**
@@ -224,8 +175,8 @@ public class PhetApplication
      *
      * @param module the Module to add.
      */
-    public void addModule( Module module ) {
-        moduleManager.addModule( module );
+    public void addModule(Module module) {
+        moduleManager.addModule(module);
     }
 
     /**
@@ -234,8 +185,8 @@ public class PhetApplication
      * @param i the Module index
      * @return the Module.
      */
-    public Module moduleAt( int i ) {
-        return moduleManager.moduleAt( i );
+    public Module moduleAt(int i) {
+        return moduleManager.moduleAt(i);
     }
 
     /**
@@ -245,8 +196,8 @@ public class PhetApplication
      * @param i
      * @return the module
      */
-    public Module getModule( int i ) {
-        return moduleAt( i );
+    public Module getModule(int i) {
+        return moduleAt(i);
     }
 
     /**
@@ -254,8 +205,8 @@ public class PhetApplication
      *
      * @param module the module to activate.
      */
-    public void setActiveModule( Module module ) {
-        moduleManager.setActiveModule( module );
+    public void setActiveModule(Module module) {
+        moduleManager.setActiveModule(module);
     }
 
     /**
@@ -263,8 +214,8 @@ public class PhetApplication
      *
      * @param i the module index to activate.
      */
-    public void setActiveModule( int i ) {
-        moduleManager.setActiveModule( i );
+    public void setActiveModule(int i) {
+        moduleManager.setActiveModule(i);
     }
 
     /**
@@ -276,10 +227,9 @@ public class PhetApplication
      */
     public Module getStartModule() {
         Option<Integer> developmentModule = getDevelopmentModule();
-        if ( developmentModule.isSome() ) {
-            return moduleAt( developmentModule.get() );
-        }
-        else {
+        if (developmentModule.isSome()) {
+            return moduleAt(developmentModule.get());
+        } else {
             return moduleManager.getStartModule();
         }
     }
@@ -289,8 +239,8 @@ public class PhetApplication
      *
      * @param module
      */
-    public void setStartModule( Module module ) {
-        moduleManager.setStartModule( module );
+    public void setStartModule(Module module) {
+        moduleManager.setStartModule(module);
     }
 
     /**
@@ -298,8 +248,8 @@ public class PhetApplication
      *
      * @param moduleObserver
      */
-    public void addModuleObserver( ModuleObserver moduleObserver ) {
-        moduleManager.addModuleObserver( moduleObserver );
+    public void addModuleObserver(ModuleObserver moduleObserver) {
+        moduleManager.addModuleObserver(moduleObserver);
     }
 
     /**
@@ -307,8 +257,8 @@ public class PhetApplication
      *
      * @param moduleObserver the ModuleObserver to unsubscribe from module events
      */
-    public void removeModuleObserver( ModuleObserver moduleObserver ) {
-        moduleManager.removeModuleObserver( moduleObserver );
+    public void removeModuleObserver(ModuleObserver moduleObserver) {
+        moduleManager.removeModuleObserver(moduleObserver);
     }
 
     /**
@@ -317,8 +267,8 @@ public class PhetApplication
      * @param m
      * @return the index of the specified Module.
      */
-    public int indexOf( Module m ) {
-        return moduleManager.indexOf( m );
+    public int indexOf(Module m) {
+        return moduleManager.indexOf(m);
     }
 
     /**
@@ -344,9 +294,9 @@ public class PhetApplication
      *
      * @param m the array of modules to add
      */
-    public void addModules( Module[] m ) {
-        for ( int i = 0; i < m.length; i++ ) {
-            addModule( m[i] );
+    public void addModules(Module[] m) {
+        for (int i = 0; i < m.length; i++) {
+            addModule(m[i]);
         }
     }
 
@@ -377,20 +327,21 @@ public class PhetApplication
      * Displays an About Dialog for the simulation.
      */
     public void showAboutDialog() {
-        if ( aboutDialog == null ) {
-            aboutDialog = new PhetAboutDialog( this );
-            aboutDialog.addWindowListener( new WindowAdapter() {
+        if (aboutDialog == null) {
+            aboutDialog = new PhetAboutDialog(this);
+            aboutDialog.addWindowListener(new WindowAdapter() {
                 // called when the close button in the dialog's window dressing is clicked
-                public void windowClosing( WindowEvent e ) {
+
+                public void windowClosing(WindowEvent e) {
                     aboutDialog.dispose();
                 }
 
                 // called by JDialog.dispose
-                public void windowClosed( WindowEvent e ) {
+                public void windowClosed(WindowEvent e) {
                     aboutDialog = null;
                 }
-            } );
-            aboutDialog.setVisible( true );
+            });
+            aboutDialog.setVisible(true);
         }
     }
 
@@ -399,11 +350,11 @@ public class PhetApplication
      *
      * @param color
      */
-    public void setControlPanelBackground( Color color ) {
-        for ( Module module : getModules() ) {
-            module.setControlPanelBackground( color );
-            module.setClockControlPanelBackground( color );
-            module.setHelpPanelBackground( color );
+    public void setControlPanelBackground(Color color) {
+        for (Module module : getModules()) {
+            module.setControlPanelBackground(color);
+            module.setClockControlPanelBackground(color);
+            module.setHelpPanelBackground(color);
         }
     }
 
@@ -426,7 +377,7 @@ public class PhetApplication
      *
      * @param exitStrategy the new exit strategy to be applied on sim exit
      */
-    public void setExitStrategy( VoidFunction0 exitStrategy ) {
+    public void setExitStrategy(VoidFunction0 exitStrategy) {
         this.exitStrategy = exitStrategy;
     }
 
@@ -446,18 +397,18 @@ public class PhetApplication
     public Option<Integer> getDevelopmentModule() {
 
         //Make sure the sim is running in development mode
-        if ( phetApplicationConfig.isDev() ) {
+        if (phetApplicationConfig.isDev()) {
 
             //Check the command line arguments for presence of "-module" command
-            int index = Arrays.asList( phetApplicationConfig.getCommandLineArgs() ).indexOf( "-module" );
-            if ( index >= 0 && index + 1 < phetApplicationConfig.getCommandLineArgs().length ) {
+            int index = Arrays.asList(phetApplicationConfig.getCommandLineArgs()).indexOf("-module");
+            if (index >= 0 && index + 1 < phetApplicationConfig.getCommandLineArgs().length) {
 
                 //Find the next argument and parse
                 String moduleIndexString = phetApplicationConfig.getCommandLineArgs()[index + 1];
-                int moduleIndex = parseInt( moduleIndexString );
+                int moduleIndex = parseInt(moduleIndexString);
 
                 //Signify the selected module
-                return new Option.Some<Integer>( moduleIndex );
+                return new Option.Some<Integer>(moduleIndex);
             }
         }
 
